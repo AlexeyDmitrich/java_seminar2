302c716de236a5cdfc9ca692e5c1886825897ea9
Alexey <a.d.kamenev@yandex.ru>
2023-03-10 09:58:37 +0300

    HomeTask1 complete

--- src/HomeTask1.java
+++ src/HomeTask1.java
@@ -1,10 +1,21 @@
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.*;
 
 public class HomeTask1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int length = input.Int("Задайте длину массива:\n");
         int[] usersArray = fillArray(new int[length]);
-        int[] newArr = sortArray(usersArray);
+
+        Logger logger = Logger.getLogger(HomeTask1.class.getName());
+        logger.setLevel(Level.INFO);
+        FileHandler ch = new FileHandler ("log.xml");
+        logger.addHandler (ch);
+        SimpleFormatter sFormat = new SimpleFormatter ();
+        XMLFormatter xml = new XMLFormatter();
+        ch.setFormatter (xml);
+
+        int[] newArr = sortArray(usersArray, logger);
         System.out.println(Arrays.toString(newArr));
     }
 
@@ -15,11 +26,12 @@ public class HomeTask1 {
         return usersArray;
     }
 
-    public static int[] sortArray (int[]unsortedArray){
+    public static int[] sortArray (int[]unsortedArray, Logger logger){
+
+        logger.info("Запускаем сортировку");
+
         int[] sortedArr = unsortedArray;
-        int stepUp = unsortedArray[unsortedArray.length-1];
-        int stepDown = unsortedArray[0];
-        System.out.println(Arrays.toString(unsortedArray));
+        int stepUp;
         int counter=0;
         for (int i = 0; i < unsortedArray.length-1; i++) {
             if (unsortedArray[i]>unsortedArray[i+1]){
@@ -28,12 +40,14 @@ public class HomeTask1 {
                 unsortedArray[i] = stepUp;
                 counter++;
                 System.out.println(Arrays.toString(unsortedArray));
+
             }
             sortedArr[i] = unsortedArray[i];
+            logger.log(Level.WARNING, Arrays.toString(sortedArr) );
         }
         if (counter == 0){
             return sortedArr;
         }
-        return sortArray(sortedArr);
+        return sortArray(sortedArr, logger);
     }
 }
